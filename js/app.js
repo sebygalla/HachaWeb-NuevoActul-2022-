f6b2c8f0243097467953d38f34cecbb585de74a9
sebygalla <sebygalla@hotmail.com>
2021-10-21 08:28:27 -0300

    menu fixed
    menu fixed

--- js/app.js
+++ js/app.js
@@ -3,7 +3,26 @@
 
 const btnHamb= document.getElementById('hamb');
 const menu= document.getElementById('menu');
+const enlace= document.querySelector('.menu');
+
+
+
+
 btnHamb.addEventListener('click', ()=>{
     menu.classList.toggle('menu-on')
+
+  
      
-})
+});
+
+
+/*al hacer click sobre un enlace se dirije a el y desaparece la lista de enlaces)*/
+
+enlace.addEventListener('click', ()=>{
+    
+    
+    enlace.classList.remove("menu-on");
+
+});
+
+
