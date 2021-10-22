bd683594221c0bc99d44db99efb962a40b521779
sebygalla <sebygalla@hotmail.com>
2021-10-22 17:26:51 -0300

    barra scroll y boton de ir arriba

--- js/app.js
+++ js/app.js
@@ -25,3 +25,37 @@ enlace.addEventListener('click', () => {
 });
 
 
+/*BOTON DE IR ARRIBA Y BARRA SUPERIOR DE SCROLL*/
+
+addEventListener('DOMContentLoaded', () => {
+
+    const boton_ir_arriba = document.querySelector('#ir_arriba_boton')
+    const barra_indicador = document.querySelector('.indicador_scroll')
+
+    const obtener_pixeles_inicio = () => document.documentElement.scrollTop || document.body.scrollTop
+
+    const irArriba = () => {
+
+        if (obtener_pixeles_inicio() > 0) {
+            requestAnimationFrame(irArriba)
+            scrollTo(0, obtener_pixeles_inicio() - (obtener_pixeles_inicio() / 2))
+        }
+    }
+
+    const indicarScroll = () => {
+
+        if (obtener_pixeles_inicio() > 50) {
+            boton_ir_arriba.className = 'mostrar'
+        } else {
+            boton_ir_arriba.className = 'ocultar'
+        }
+
+        let alto = document.documentElement.scrollHeight - document.documentElement.clientHeight
+        
+        let avance_scroll = (obtener_pixeles_inicio() / alto) * 100
+        barra_indicador.style.width = avance_scroll + '%'
+    }
+
+    boton_ir_arriba.addEventListener('click', irArriba)
+    window.addEventListener('scroll', indicarScroll)
+})
