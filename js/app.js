0efb88cf212b846332949397ad1ae9f80a22b575
sebygalla <sebygalla@hotmail.com>
2021-10-22 08:28:20 -0300

    sombras
    filter drop-shadow

--- js/app.js
+++ js/app.js
@@ -1,26 +1,25 @@
-
 /* MENU HAMBURGUESA*/
 
-const btnHamb= document.getElementById('hamb');
-const menu= document.getElementById('menu');
-const enlace= document.querySelector('.menu');
+const btnHamb = document.getElementById('hamb');
+const menu = document.getElementById('menu');
+const enlace = document.querySelector('.menu');
 
 
 
 
-btnHamb.addEventListener('click', ()=>{
+btnHamb.addEventListener('click', () => {
     menu.classList.toggle('menu-on')
 
-  
-     
+
+
 });
 
 
 /*al hacer click sobre un enlace se dirije a el y desaparece la lista de enlaces)*/
 
-enlace.addEventListener('click', ()=>{
-    
-    
+enlace.addEventListener('click', () => {
+
+
     enlace.classList.remove("menu-on");
 
 });
