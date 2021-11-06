b54acfbc53b7eeee9befb052b44443f7f1f74423
sebygalla <sebygalla@hotmail.com>
2021-11-06 16:52:52 -0300

    modal responsive

--- js/app.js
+++ js/app.js
@@ -33,7 +33,7 @@ btnArriba.addEventListener('click', () => {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
 
-    
+
 });
 
 window.addEventListener('scroll', () => {
@@ -41,5 +41,43 @@ window.addEventListener('scroll', () => {
         btnArriba.style.display = 'block';
     } else {
         btnArriba.style.display = 'none';
+    }
+});
+
+
+
+/*MODAL*/
+
+let cerrar = document.querySelectorAll('.close')[0];
+let abrir = document.querySelectorAll('.button')[0];
+let modal = document.querySelectorAll('.modal')[0];
+let modalC = document.querySelectorAll('.modal-container')[0];
+
+abrir.addEventListener('click', function (e) {
+    e.preventDefault();
+    modalC.style.opacity = '1';
+    modalC.style.visibility = 'visible';
+    modal.classList.toggle('modal-close');
+});
+
+cerrar.addEventListener('click', function () {
+    modal.classList.toggle('modal-close');
+
+    setTimeout(function () {
+        modalC.style.opacity = '0';
+        modalC.style.visibility = 'hidden';
+
+    }, 850);
+});
+
+window.addEventListener('click', function (e) {
+    if (e.target == modalC) {
+        modal.classList.toggle('modal-close');
+
+        setTimeout(function () {
+            modalC.style.opacity = '0';
+            modalC.style.visibility = 'hidden';
+
+        }, 850);
     }
 });
